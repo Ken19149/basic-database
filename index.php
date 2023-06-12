0678738d786e4cba1b2f3139d5dfa4c44ef91a78
Ken980 <kenboy9800@gmail.com>
2023-06-13 01:04:52 +0700

    add form to index.php
    - also add favicon

--- index.php
+++ index.php
@@ -16,9 +16,18 @@ while ($data = mysqli_fetch_array($query)) {
     <meta charset="utf-8">
     <meta name="viewport" content="width-device-width, initial-scale=1">
     <title>DATABASE TESTING</title>
+    <link rel='icon' href='favicon.ico' type='image/x-icon'> 
+    <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'>
 </head>
 
 <body>
-
+    <form action="add.php" method="post">
+        <br>
+        Input ID: <input type="text" name="id"><br>
+        Product Name: <input type="text" name="product_name"><br>
+        Price: <input type="text" name="price"><br>
+        Status: <input type="text" name="status"><br>
+        <input type="submit" value="insert info">
+    </form>
 </body>
 </html>
